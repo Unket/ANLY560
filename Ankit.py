98d2ff745ca9d303facb4bcef0f89c061bcd677e
Unket <44682933+Unket@users.noreply.github.com>
2020-01-30 20:27:33 -0500

    ANLY 560

---
+++ Ankit.py
@@ -0,0 +1,10 @@
+print("Area and peremeter of Rectangale")
+width = float(input('Please Enter the Width of a Rectangle: '))
+length = float(input('Please Enter the Length of a Rectangle: '))
+
+Area = width * length
+Perimeter = 2 * (width + length)
+
+print("\n Area of a Rectangle is: %.2f" %Area)
+print(" Perimeter of Rectangle is: %.2f" %Perimeter)
+
